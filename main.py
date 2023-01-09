79b64ef6f833ec1008d027cc22ad83b05d2b1356
Jennifer-Dong <jenndong12016@gmail.com>
2023-01-08 22:21:14 -0500

    yay

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-'''
 import re
 
-with open('followers.html') as file:
+with open("C:/Users/jennd/Desktop/Work/Projects/IGNotFollowingBack/IGNotFollowingBack/followers.html") as file:
     for line in file:
-        urls = re.findall('https?://\S+)', line)
-        print(urls)
+            followers = re.findall('https://www.instagram.com/[^"]+', line)
 
-'''
+with open("C:/Users/jennd/Desktop/Work/Projects/IGNotFollowingBack/IGNotFollowingBack/following.html") as file:
+    for line in file:
+            following = re.findall('https://www.instagram.com/[^"]+', line)
 
-f=open('followers.html', 'r')
-print(f.read(5))
+uh_oh = []
+what = []
+
+for i in following:
+    if i not in followers:
+        uh_oh.append(i)
+for i in followers:
+    if i not in following:
+        what.append(i)
+
+print('Not Following You, But You Follow Them')
+for i in range(len(uh_oh)):
+    print(i+1, uh_oh[i])
+print('\n\nFollowing You, But You Don\'t Follow Them')
+for i in range(len(what)):
+    print(i+1, what[i])
+
